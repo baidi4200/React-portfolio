fddddbdbe4ad11ddb79e332bcd43ad61b596060f
obaid <obaidobaid345@gmail.com>
2021-11-27 17:45:10 +0500

    Updates

---
+++ precache-manifest.3bccd15435e5d3fb475034447d3238bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9e2a8c0041b0a8df975c696f95b223",
+    "url": "/React-portfolio/index.html"
+  },
+  {
+    "revision": "a2d6bcc9ab803a7f71af",
+    "url": "/React-portfolio/static/css/2.dbd8b1e0.chunk.css"
+  },
+  {
+    "revision": "489eb340ce300a1f6f8b",
+    "url": "/React-portfolio/static/css/main.36637314.chunk.css"
+  },
+  {
+    "revision": "a2d6bcc9ab803a7f71af",
+    "url": "/React-portfolio/static/js/2.a46e896c.chunk.js"
+  },
+  {
+    "revision": "c40be130369555ab6cc7391c28632b27",
+    "url": "/React-portfolio/static/js/2.a46e896c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "489eb340ce300a1f6f8b",
+    "url": "/React-portfolio/static/js/main.8dc07926.chunk.js"
+  },
+  {
+    "revision": "a1fedd6559cdd85f178c",
+    "url": "/React-portfolio/static/js/runtime-main.9dd9595f.js"
+  }
+]);
